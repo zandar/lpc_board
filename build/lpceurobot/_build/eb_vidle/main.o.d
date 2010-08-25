4d2b679dd59d4bf4cf37a3ff9ce97435c80c92f1
Michal Vokac <vokac.m@gmail.com>
2010-08-25 16:55:04 +0200

    LCD module is working, problem with wait function in lcd.h solved. Added function lcd_gotoxy() to set cursor position.

--- build/lpceurobot/_build/eb_vidle/main.o.d
+++ build/lpceurobot/_build/eb_vidle/main.o.d
@@ -2,22 +2,9 @@ main.o: \
  /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/eb_vidle/main.c \
  /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/lpc21xx.h \
  /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/types.h \
+ /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/system_def.h \
  /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/deb_led.h \
  /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/deb_led_board.h \
- /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/system_def.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/string.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/_ansi.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/newlib.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/config.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/machine/ieeefp.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/reent.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/_ansi.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/_types.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/machine/_types.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/machine/_default_types.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/lock.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/include/stddef.h \
- /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/string.h \
  /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/eb_vidle/uar.h \
  /usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/include/stdbool.h \
  /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/eb_vidle/def.h \
@@ -28,37 +15,11 @@ main.o: \
 
 /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/types.h:
 
+/media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/system_def.h:
+
 /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/deb_led.h:
 
 /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/deb_led_board.h:
-
-/media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/_compiled/include/system_def.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/string.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/_ansi.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/newlib.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/config.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/machine/ieeefp.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/reent.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/_ansi.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/_types.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/machine/_types.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/machine/_default_types.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/lock.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/include/stddef.h:
-
-/usr/arm-elf/gcc/4.4.4/bin/../lib/gcc/arm-elf/4.4.4/../../../../arm-elf/include/sys/string.h:
 
 /media/DATA/programovani/C_mikrokontroler/lpc_board/build/lpceurobot/eb_vidle/uar.h:
 
